fd574440edf639a4b2dcb8cba4f8c5bd1b3d730c
Declan W <148910875+DeclanW1@users.noreply.github.com>
2025-11-18 12:05:21 +0000

    Added collision between player and walls

--- Engine/World.cs
+++ Engine/World.cs
@@ -1,39 +1,117 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
 
 namespace MiniGame.Engine
 {
     public class World
     {
-        private readonly List<GameObject> objects = new List<GameObject>();
+        //all game objects in the world
+        private readonly List<GameObject> _objects = new List<GameObject>();
 
         public void Add(GameObject obj)
         {
-            objects.Add(obj);
+            _objects.Add(obj);
         }
 
         public void Update(GameTime gameTime)
         {
-            foreach (var obj in objects)
+            foreach (var obj in _objects)
             {
                 if (obj.IsActive)
                 {
                     obj.Update(gameTime);
                 }
             }
+
+            HandlePlayerWallCollisions();
         }
 
+        //draw all the active objects
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach (var obj in objects)
+            foreach (var obj in _objects)
             {
                 if (obj.IsActive)
                 {
                     obj.Draw(spriteBatch);
                 }
             }
+        }
+
+        // COLLISIONS -- Player + Wall
+        private void HandlePlayerWallCollisions()
+        {
+            var players = new List<Player>();
+            var walls = new List<Wall>();
+
+            //objects categorised into players and walls
+            foreach (var obj in _objects)
+            {
+                if (obj is Player p)
+                    players.Add(p);
+                else if (obj is Wall w)
+                    walls.Add(w);
+            }
+
+            //check collisions
+            foreach (var player in players)
+            {
+                foreach (var wall in walls)
+                {
+                    ResolveCollision(player, wall);
+                }
+            }
+        }
+
+        //Push the player out of any wall they overlap
+        private void ResolveCollision(Player player, Wall wall)
+        {
+            Rectangle p = ToRect(player);
+            Rectangle w = ToRect(wall);
+
+            if (!p.Intersects(w))
+                return;
+
+            Vector2 pCenter = new Vector2(p.Center.X, p.Center.Y);
+            Vector2 wCenter = new Vector2(w.Center.X, w.Center.Y);
+
+            float dx = pCenter.X - wCenter.X;
+            float dy = pCenter.Y - wCenter.Y;
+
+            //overlap amount on each axis
+            float overlapX = (p.Width / 2f + w.Width / 2f) - Math.Abs(dx);
+            float overlapY = (p.Height / 2f + w.Height / 2f) - Math.Abs(dy);
+
+            if (overlapX <= 0 || overlapY <= 0)
+                return;
+
+            if (overlapX < overlapY)
+            {
+                if (dx < 0)
+                    player.Position.X -= overlapX;
+                else
+                    player.Position.X += overlapX;
+            }
+            else
+            {
+                if (dy < 0)
+                    player.Position.Y -= overlapY;
+                else
+                    player.Position.Y += overlapY;
+            }
+        }
+
+        //converts GameObject into rectangle
+        private Rectangle ToRect(GameObject obj)
+        {
+            return new Rectangle(
+                (int)obj.Position.X,
+                (int)obj.Position.Y,
+                (int)obj.Size.X,
+                (int)obj.Size.Y
+            );
         }
     }
 }
--- obj/Debug/net9.0/EngineDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/EngineDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EngineDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dca14a1513da7845206843ec93d0f9fa26fd0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187ab2f2912df10e1024f6101585e5c0e16b30eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EngineDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EngineDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
